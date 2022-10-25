d3d71b558c876180e989d18c5ae79ffb140b2e57
Morinslash <sebastian.elf.kulinski@gmail.com>
2022-10-25 16:00:27 +0200

    Add new test cases

--- DddInPractice.Logic/Money.cs
+++ DddInPractice.Logic/Money.cs
@@ -2,12 +2,19 @@ namespace DddInPractice.Logic;
 
 public class Money : ValueObject<Money>
 {
-    public int OneCentCount { get; private set; }
-    public int TenCentCount { get; private set; }
-    public int QuarterCentCount { get; private set; }
-    public int OneDollarCount { get; private set; }
-    public int FiveDollarCount { get; private set; }
-    public int TwentyDollarCount { get; private set; }
+    public int OneCentCount { get; }
+    public int TenCentCount { get; }
+    public int QuarterCentCount { get; }
+    public int OneDollarCount { get; }
+    public int FiveDollarCount { get; }
+    public int TwentyDollarCount { get; }
+    public decimal Amount =>
+        OneCentCount * 0.01m +
+        TenCentCount * 0.10m +
+        QuarterCentCount * 0.25m +
+        OneDollarCount +
+        FiveDollarCount * 5 +
+        TwentyDollarCount * 20;
 
     public Money(int oneCentCount, int tenCentCount, int quarterCentCount, int oneDollarCount, int fiveDollarCount,
         int twentyDollarCount)
@@ -61,6 +68,16 @@ public class Money : ValueObject<Money>
             money1.TwentyDollarCount + money2.TwentyDollarCount
         );
         return sum;
+    }
+
+    public static Money operator -(Money money1, Money money2)
+    {
+        return new Money(money1.OneCentCount - money2.OneCentCount,
+            money1.TenCentCount - money2.TenCentCount,
+            money1.QuarterCentCount - money2.QuarterCentCount,
+            money1.OneDollarCount - money2.OneDollarCount,
+            money1.FiveDollarCount - money2.FiveDollarCount,
+            money1.TwentyDollarCount - money2.TwentyDollarCount);
     }
 
     protected override bool EqualsCore(Money other) =>
--- DddInPractice.Tests/MoneySpecs.cs
+++ DddInPractice.Tests/MoneySpecs.cs
@@ -54,4 +54,51 @@ public class MoneySpecs
 
         action.Should().Throw<InvalidOperationException>();
     }
+    
+    [Theory]
+    [InlineData(0,0,0,0,0,0,0)]
+    [InlineData(1,0,0,0,0,0,0.01)]
+    [InlineData(1,2,0,0,0,0,0.21)]
+    [InlineData(1,2,3,0,0,0, 0.96)]
+    [InlineData(1,2,3,4,0,0,4.96)]
+    [InlineData(1,2,3,4,5,0,29.96)]
+    [InlineData(1,2,3,4,5,6,149.96)]
+    [InlineData(11,0,0,0,0,0,0.11)]
+    [InlineData(110,0,0,0,100,0,501.1)]
+    public void Amount_Is_Calculated_Correctly(int oneCent, int tenCent, int quarter, int oneDollar,
+        int fiveDollar, int twentyDollar, decimal expectedAmount)
+    {
+        var money = new Money(oneCent, tenCent, quarter, oneDollar, fiveDollar, twentyDollar);
+        money.Amount.Should().Be(expectedAmount);
+    }
+
+    [Fact]
+    public void Subtraction_Of_Two_Moneys_Produces_Correct_Result()
+    {
+        var money1 = new Money(10,10,10,10,10,10);
+        var money2 = new Money(1,2,3,4,5,6);
+
+        var result = money1 - money2;
+        
+        result.OneCentCount.Should().Be(9);
+        result.TenCentCount.Should().Be(8);
+        result.QuarterCentCount.Should().Be(7);
+        result.OneDollarCount.Should().Be(6);
+        result.FiveDollarCount.Should().Be(5);
+        result.TwentyDollarCount.Should().Be(4);
+    }
+
+    [Fact]
+    public void Cannot_Subtract_More_Than_Exists()
+    {
+        var money1 = new Money(0,1,0,0,0,0);
+        var money2 = new Money(1,0,0,0,0,0);
+
+        Action action = () =>
+        {
+            Money money = money1 - money2;
+        };
+
+        action.Should().Throw<InvalidOperationException>();
+    }
 }
